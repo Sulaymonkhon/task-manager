2598e3885a2d0b9b6032a015353ff11eb8f8f34a
Sulaymon Khamrakhujaev <sulaymonkhamrakhujaev@gmail.com>
2022-12-24 21:54:05 +0300

    Added static folder for frontend files in public folder. Made a simple, starting ui for the project using bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import express from "express";
+import path    from "path";
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log("Server started on port 3000. http://localhost:3000. Press Ctrl+C to exit.");
